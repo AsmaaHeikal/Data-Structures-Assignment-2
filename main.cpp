63e87f98332d7a6e0c159daeb60629fe6b885169
Laila Kandil <101605144+LailaKandil@users.noreply.github.com>
2023-05-01 14:19:44 +0200

    Merge K sorted linked lists

--- main.cpp
+++ main.cpp
@@ -1,91 +1,69 @@
 #include <iostream>
-#include <vector>
-#include<algorithm>
+#include <queue>
 using namespace std;
-template<class T>
-class stack {
-private:
-    vector<T> stck;
-    int sz = 0;
-
-public:
-
-    void push(T item) {
-        stck.push_back(item);
-    }
-
-    T top() {
-        return stck.back();
-    }
-
-    T pop() {
-        T el;
-        if (stck.size() > 0) {
-            el = stck.back();
-            stck.pop_back();
-            sz--;
-            return el;
-        } else cout << "Cannot pop since the stack is empty" << endl;
-
-    }
-
-    int stackSize() {
-        sz =0;
-        for (auto i: stck) {
-            sz++;
-        }
-        return sz;
-    }
-
-    bool isEmpty() {
-        if (stck.empty()) {
-            return true;
-        } else return false;
-    }
-
-    void print() {
-        if (stck.empty()) {
-            cout << "Stack is empty" << endl;
-        } else {
-            for (auto i: stck) {
-                cout << i << " ";
-            }
-            cout << endl;
-        }
-    }
-
-    void clear() {
-
-        if (!stck.empty()) {
-            for (auto i: stck) {
-                stck.pop_back();
-            }
-            sz = 0;
-        } else cout << "The stack is already empty";
-
-    }
-
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int a) : val(a), next(NULL) {}
 };
-int LongestValidParenthesis(string s){
-    stack<int> st;
-    int maxLength=0;
-    st.push(-1);
-    for (int i=0;i<s.length();i++) {
-        if (s[i] == '(') {
-            st.push(i);
-        }
-        else{
-            st.pop();
-            if(st.isEmpty())
-                st.push(i);
-            else{
-                int len=i-st.top();
-                maxLength= max(maxLength,len);
-            }
+
+struct CompareNodes{
+    bool operator()(const ListNode* x, const ListNode* y) {
+        return x->val > y->val;
+    }
+};
+
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    priority_queue<ListNode*, vector<ListNode*>, CompareNodes> pq;
+
+    // Push the first node of each list onto the priority queue
+    for (ListNode* list : lists) {
+        if (list != NULL) {
+            pq.push(list);
         }
     }
-    return maxLength;
+
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+
+    // Pop the smallest node from the priority queue and append it to the merged list
+    while (!pq.empty()) {
+        ListNode* node = pq.top();
+        pq.pop();
+        tail->next = node;
+        tail = node;
+
+        // Push the next node of the merged list onto the priority queue
+        if (node->next != NULL) {
+            pq.push(node->next);
+        }
+    }
+
+    return dummy.next;
 }
-int main(){
-    cout<<LongestValidParenthesis(")()())");
+int main() {
+    // Create three linked lists
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(4);
+    list1->next->next = new ListNode(5);
+
+    ListNode* list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);
+
+    ListNode* list3 = new ListNode(2);
+    list3->next = new ListNode(6);
+
+    // Store the head nodes of the linked lists in a vector
+    vector<ListNode*> lists = {list1, list2, list3};
+
+    // Merge the linked lists and print the result
+    ListNode* merged = mergeKLists(lists);
+
+    while (merged != NULL) {
+        cout << merged->val << " ";
+        merged = merged->next;
+    }
+    cout << endl;
+    return 0;
 }
